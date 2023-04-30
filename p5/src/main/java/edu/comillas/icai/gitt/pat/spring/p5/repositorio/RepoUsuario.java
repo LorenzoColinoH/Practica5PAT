be7cc10fc764a5f78d12a7bcb2415f1d4fe195a5
LorenzoColinoH <108193604+LorenzoColinoH@users.noreply.github.com>
2023-04-30 22:05:18 +0200

    Delete p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/repositorio directory

--- p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/repositorio/RepoUsuario.java
+++
@@ -1,8 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.p5.repositorio;
-
-import edu.comillas.icai.gitt.pat.spring.p5.entidad.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepoUsuario extends CrudRepository<Usuario, Long> {
-    Usuario findByCredenciales(String credenciales);
-}
